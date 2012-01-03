827a47b31937ad4fce8de9142ca126c74259796b
Jim Graham <jim@jim-graham.net>
2012-01-03 18:11:38 -0500

    clean up and documentation

---
+++ NetSparkle/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppLimit.NetSparkle
+{
+    /// <summary>
+    /// Everytime when netsparkle detects an update the 
+    /// consumer can decide what should happen as next with the help 
+    /// of the UpdateDatected event
+    /// </summary>
+    public enum NextUpdateAction
+    {
+        /// <summary>
+        /// Show the user interface
+        /// </summary>
+        ShowStandardUserInterface = 1,
+        /// <summary>
+        /// Perform an unattended install
+        /// </summary>
+        PerformUpdateUnattended = 2,
+        /// <summary>
+        /// Prohibit the update
+        /// </summary>
+        ProhibitUpdate = 3
+    }
+}
