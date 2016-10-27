4436d0ed12db550708e9901c0158a61f41491d7b
Deadpikle <mikebobby@sbcglobal.net>
2016-10-27 13:11:20 -0400

    WPF Sample project now actually works!

--- TestAppFiles/GenerateHashForDemoSoftware.bat
+++ TestAppFiles/GenerateHashForDemoSoftware.bat
@@ -1,16 +1,16 @@
 @echo off
 if exist "../bin/Release/NetSparkle.DSAHelper.exe" (
-	if exist "../bin/Release/SampleDownloadedExecutable.exe" (
-		"../bin/Release/NetSparkle.DSAHelper.exe" /sign_update "../bin/Release/SampleDownloadedExecutable.exe" NetSparkle_DSA.priv > hash-demo-file-release.txt
+	if exist "../bin/Release/NetSparkleUpdate.exe" (
+		"../bin/Release/NetSparkle.DSAHelper.exe" /sign_update "../bin/Release/NetSparkleUpdate.exe" NetSparkle_DSA.priv > hash-demo-file-release.txt
 	)
-	if exist "../bin/Debug/SampleDownloadedExecutable.exe" (
-		"../bin/Release/NetSparkle.DSAHelper.exe" /sign_update "../bin/Debug/SampleDownloadedExecutable.exe" NetSparkle_DSA.priv > hash-demo-file-debug.txt
+	if exist "../bin/Debug/NetSparkleUpdate.exe" (
+		"../bin/Release/NetSparkle.DSAHelper.exe" /sign_update "../bin/Debug/NetSparkleUpdate.exe" NetSparkle_DSA.priv > hash-demo-file-debug.txt
 	)
 ) else if exist "../bin/Debug/NetSparkle.DSAHelper.exe" (
-	if exist "../bin/Release/NetSparkle.TestApp.exe" (
-		"../bin/Debug/NetSparkle.DSAHelper.exe" /sign_update "../bin/Release/SampleDownloadedExecutable.exe" NetSparkle_DSA.priv > hash-demo-file-release.txt
+	if exist "../bin/Release/NetSparkleUpdate.exe" (
+		"../bin/Debug/NetSparkle.DSAHelper.exe" /sign_update "../bin/Release/NetSparkleUpdate.exe" NetSparkle_DSA.priv > hash-demo-file-release.txt
 	) 
-	if exist "../bin/Debug/NetSparkle.TestApp.exe" (
-		"../bin/Debug/NetSparkle.DSAHelper.exe" /sign_update "../bin/Debug/SampleDownloadedExecutable.exe" NetSparkle_DSA.priv > hash-demo-file-debug.txt
+	if exist "../bin/Debug/NetSparkleUpdate.exe" (
+		"../bin/Debug/NetSparkle.DSAHelper.exe" /sign_update "../bin/Debug/NetSparkleUpdate.exe" NetSparkle_DSA.priv > hash-demo-file-debug.txt
 	)
 )
