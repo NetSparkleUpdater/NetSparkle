0cd4528f2ae60ad1965c336d21ebf43025e0672a
ndreisg <a.grossauer@gmail.com>
2019-01-29 20:01:26 +0100

    fixed indentation
    indentation with 4 spaces instead of 2

--- NetSparkleGenerator/AppCastXMLItem.cs
+++ NetSparkleGenerator/AppCastXMLItem.cs
@@ -3,69 +3,69 @@ using System.Xml;
 
 namespace NetSparkleGenerator
 {
-  class AppCastXMLItem
-  {
-    public string Title;
-    public string DownloadLink;
-    public string Version;
-    public DateTime? PublicationDate;
-    public string ShortVersion;
-    public string ReleaseNotesLink;
-    public string Length;
-    public string Type;
-    public string DsaSignature;
-
-    public AppCastXMLItem(string title, string downloadLink, string version, DateTime? pubDate, string shortVersion = "", string releaseNotesLink = "", string length = "0", string type = "application/octet-stream", string dsaSignature = "")
+    class AppCastXMLItem
     {
-      Title = title;
-      DownloadLink = downloadLink;
-      Version = version;
-      PublicationDate = pubDate;
-      ShortVersion = shortVersion;
-      ReleaseNotesLink = releaseNotesLink;
-      Length = length;
-      Type = type;
-      DsaSignature = dsaSignature;
-    }
+        public string Title;
+        public string DownloadLink;
+        public string Version;
+        public DateTime? PublicationDate;
+        public string ShortVersion;
+        public string ReleaseNotesLink;
+        public string Length;
+        public string Type;
+        public string DsaSignature;
 
-    public void WriteItem(XmlWriter writer)
-    {
-      writer.WriteStartElement("item");
-      writer.WriteElementString("title", Title);
-
-      if (!String.IsNullOrEmpty(ReleaseNotesLink))
-      {
-        writer.WriteElementString("sparkle", "releaseNotesLink", AppCastXMLGenerator.xmlns, ReleaseNotesLink);
-      }
-
-      if (PublicationDate.HasValue && PublicationDate.Value != DateTime.MinValue && PublicationDate.Value != DateTime.MaxValue)
-      {
-        //writer.WriteElementString("pubDate", PublicationDate.Value.ToString("r"));
-        writer.WriteElementString("pubDate", PublicationDate.Value.ToString("ddd, dd MMM yyyy HH:mm:ss zzz", System.Globalization.CultureInfo.InvariantCulture));
-      }
-
-      if (!String.IsNullOrEmpty(DownloadLink))
-      {
-        writer.WriteStartElement("enclosure");
-        writer.WriteAttributeString("url", DownloadLink);
-        writer.WriteAttributeString("sparkle", "version", AppCastXMLGenerator.xmlns, Version);
-
-        if (!String.IsNullOrEmpty(ShortVersion))
+        public AppCastXMLItem(string title, string downloadLink, string version, DateTime? pubDate, string shortVersion = "", string releaseNotesLink = "", string length = "0", string type = "application/octet-stream", string dsaSignature = "")
         {
-          writer.WriteAttributeString("sparkle", "shortVersionString", AppCastXMLGenerator.xmlns, ShortVersion);
+            Title = title;
+            DownloadLink = downloadLink;
+            Version = version;
+            PublicationDate = pubDate;
+            ShortVersion = shortVersion;
+            ReleaseNotesLink = releaseNotesLink;
+            Length = length;
+            Type = type;
+            DsaSignature = dsaSignature;
         }
 
-        writer.WriteAttributeString("length", Length);
-        writer.WriteAttributeString("type", Type);
-
-        if (!String.IsNullOrEmpty(DsaSignature))
+        public void WriteItem(XmlWriter writer)
         {
-          writer.WriteAttributeString("sparkle", "dsaSignature", AppCastXMLGenerator.xmlns, DsaSignature);
-        }
-        writer.WriteEndElement();
-      }
+            writer.WriteStartElement("item");
+            writer.WriteElementString("title", Title);
 
-      writer.WriteEndElement();
+            if (!String.IsNullOrEmpty(ReleaseNotesLink))
+            {
+                writer.WriteElementString("sparkle", "releaseNotesLink", AppCastXMLGenerator.xmlns, ReleaseNotesLink);
+            }
+
+            if (PublicationDate.HasValue && PublicationDate.Value != DateTime.MinValue && PublicationDate.Value != DateTime.MaxValue)
+            {
+                //writer.WriteElementString("pubDate", PublicationDate.Value.ToString("r"));
+                writer.WriteElementString("pubDate", PublicationDate.Value.ToString("ddd, dd MMM yyyy HH:mm:ss zzz", System.Globalization.CultureInfo.InvariantCulture));
+            }
+
+            if (!String.IsNullOrEmpty(DownloadLink))
+            {
+                writer.WriteStartElement("enclosure");
+                writer.WriteAttributeString("url", DownloadLink);
+                writer.WriteAttributeString("sparkle", "version", AppCastXMLGenerator.xmlns, Version);
+
+                if (!String.IsNullOrEmpty(ShortVersion))
+                {
+                    writer.WriteAttributeString("sparkle", "shortVersionString", AppCastXMLGenerator.xmlns, ShortVersion);
+                }
+
+                writer.WriteAttributeString("length", Length);
+                writer.WriteAttributeString("type", Type);
+
+                if (!String.IsNullOrEmpty(DsaSignature))
+                {
+                    writer.WriteAttributeString("sparkle", "dsaSignature", AppCastXMLGenerator.xmlns, DsaSignature);
+                }
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+        }
     }
-  }
 }
