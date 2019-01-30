4281dccd61dd56c80e30975b163100f3e156b94e
ndreisg <a.grossauer@gmail.com>
2019-01-30 11:02:00 +0100

    Added automatic signing
    If a *.priv file is found in the same directory, it will be used to sign the .exe files and the appcast.xml

--- NetSparkleGenerator/Program.cs
+++ NetSparkleGenerator/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace NetSparkleGenerator
 {
@@ -22,7 +19,8 @@ namespace NetSparkleGenerator
                 Console.WriteLine();
                 Console.WriteLine("  e.g. generate_appcast .\\my-apps-setup-files MyApp");
                 Console.WriteLine("  (this will generate appcast.xml for all \"*MyApp*.exe\" files in the given directory)");
-                Console.WriteLine("  ");
+                Console.WriteLine();
+                Console.WriteLine(" You may place a \"*.priv\" file with private DSA key in the <directory_with_update_files> for automatic signing");
                 Environment.Exit(1);
             }
 
@@ -37,6 +35,21 @@ namespace NetSparkleGenerator
                     Console.WriteLine(ex.Message);
                     Environment.Exit(1);
                 }
+            }
+
+            DSACryptoServiceProvider cryptoProvider = null;
+            var privateKeyFilePaths = Directory.GetFiles(Environment.CurrentDirectory, "*.priv");
+            if (privateKeyFilePaths.Length > 0)
+            {
+                // Reading private key
+                String key = null;
+                using (StreamReader reader = new StreamReader(privateKeyFilePaths[0]))
+                {
+                    key = reader.ReadToEnd();
+                }
+
+                cryptoProvider = new DSACryptoServiceProvider();
+                cryptoProvider.FromXmlString(key);
             }
 
             var searchPattern = "*.exe";
@@ -55,13 +68,40 @@ namespace NetSparkleGenerator
 
             foreach (var exePath in exePaths)
             {
+                String dsaSignature = "";
+
+                if (cryptoProvider != null)
+                {
+                    // open stream
+                    Byte[] hash = null;
+                    using (Stream inputStream = File.OpenRead(exePath))
+                    {
+                        hash = cryptoProvider.SignData(inputStream);
+                    }
+
+                    dsaSignature = Convert.ToBase64String(hash);
+                }
+
                 var versionInfo = FileVersionInfo.GetVersionInfo(exePath);
                 var fileInfo = new FileInfo(exePath);
 
                 var appCastGenerator = new AppCastXMLGenerator(versionInfo.ProductName);
-                appCastGenerator.AddItem(new AppCastXMLItem(versionInfo.ProductVersion, Path.GetFileName(versionInfo.FileName), versionInfo.ProductVersion, fileInfo.CreationTime, length: fileInfo.Length.ToString()));
+                appCastGenerator.AddItem(new AppCastXMLItem(versionInfo.ProductVersion, Path.GetFileName(versionInfo.FileName), versionInfo.ProductVersion, fileInfo.CreationTime, length: fileInfo.Length.ToString(), dsaSignature: dsaSignature));
                 var appcastXmlDocument = appCastGenerator.GenerateAppCast();
                 appcastXmlDocument.Save("appcast.xml");
+
+                if (cryptoProvider != null)
+                {
+                    // open stream
+                    Byte[] hash = null;
+                    using (Stream inputStream = File.OpenRead("appcast.xml"))
+                    {
+                        hash = cryptoProvider.SignData(inputStream);
+                    }
+
+                    dsaSignature = Convert.ToBase64String(hash);
+                    File.WriteAllText("appcast.xml.dsa", dsaSignature);
+                }
             }
         }
     }
