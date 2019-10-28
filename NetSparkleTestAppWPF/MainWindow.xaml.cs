ab301375d01e2aa25eb5d50793dbd62c8ad58527
Peter Jarrett <peterj@broadcastradio.com>
2019-10-28 23:34:16 +0000

    Clarified in Sample Application that the calling application must explictly call ShowUpdateNeededUi after CheckForUpdatesQuietly

--- NetSparkleTestAppWPF/MainWindow.xaml.cs
+++ NetSparkleTestAppWPF/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace NetSparkle.TestAppWPF
         private Sparkle _sparkle;
 
         public MainWindow()
-        {           
+        {
             InitializeComponent();
 
             // remove the netsparkle key from registry 
@@ -25,9 +25,16 @@ namespace NetSparkle.TestAppWPF
             // set icon in project properties!
             string manifestModuleName = System.Reflection.Assembly.GetEntryAssembly().ManifestModule.FullyQualifiedName;
             var icon = System.Drawing.Icon.ExtractAssociatedIcon(manifestModuleName);
-            _sparkle = new Sparkle("https://deadpikle.github.io/NetSparkle/files/sample-app/appcast.xml", icon); //, "NetSparkleTestApp.exe");
+            //_sparkle = new Sparkle("https://deadpikle.github.io/NetSparkle/files/sample-app/appcast.xml", icon); //, "NetSparkleTestApp.exe");
+
+            _sparkle = new Sparkle(
+                "https://api.appcenter.ms/v0.1/public/sparkle/apps/c27e6512-ceae-4665-82c5-c0d7f6a27f30",
+                icon,
+                NetSparkle.Enums.SecurityMode.Unsafe); //, "NetSparkleTestApp.exe");
+
+
             // TLS 1.2 required by GitHub (https://developer.github.com/changes/2018-02-01-weak-crypto-removal-notice/)
-            _sparkle.SecurityProtocolType = System.Net.SecurityProtocolType.Tls12; 
+            _sparkle.SecurityProtocolType = System.Net.SecurityProtocolType.Tls12;
             _sparkle.StartLoop(true, true);
         }
 
