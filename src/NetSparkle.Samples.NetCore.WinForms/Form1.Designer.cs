5da3c1a5541b252f549baa5865701786222a7a42
Deadpikle <mbabienco@southern.edu>
2020-02-17 20:14:11 -0500

    Add .NET Core WinForms sample

---
+++ src/NetSparkle.Samples.NetCore.WinForms/Form1.Designer.cs
@@ -0,0 +1,93 @@
+namespace SampleApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkForUpdatesTimer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.AppBackgroundCheckButton = new System.Windows.Forms.Button();
+            this.ExplicitUserRequestCheckButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkForUpdatesTimer
+            // 
+            this.checkForUpdatesTimer.Enabled = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "hello";
+            this.notifyIcon1.BalloonTipTitle = "hello";
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // AppBackgroundCheckButton
+            // 
+            this.AppBackgroundCheckButton.Location = new System.Drawing.Point(12, 37);
+            this.AppBackgroundCheckButton.Name = "AppBackgroundCheckButton";
+            this.AppBackgroundCheckButton.Size = new System.Drawing.Size(212, 23);
+            this.AppBackgroundCheckButton.TabIndex = 0;
+            this.AppBackgroundCheckButton.Text = "App Background Check";
+            this.AppBackgroundCheckButton.UseVisualStyleBackColor = true;
+            this.AppBackgroundCheckButton.Click += new System.EventHandler(this.AppBackgroundCheckButton_Click);
+            // 
+            // ExplicitUserRequestCheckButton
+            // 
+            this.ExplicitUserRequestCheckButton.Location = new System.Drawing.Point(12, 80);
+            this.ExplicitUserRequestCheckButton.Name = "ExplicitUserRequestCheckButton";
+            this.ExplicitUserRequestCheckButton.Size = new System.Drawing.Size(212, 23);
+            this.ExplicitUserRequestCheckButton.TabIndex = 1;
+            this.ExplicitUserRequestCheckButton.Text = "Explicit User Request To Check ";
+            this.ExplicitUserRequestCheckButton.UseVisualStyleBackColor = true;
+            this.ExplicitUserRequestCheckButton.Click += new System.EventHandler(this.ExplicitUserRequestCheckButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 154);
+            this.Controls.Add(this.ExplicitUserRequestCheckButton);
+            this.Controls.Add(this.AppBackgroundCheckButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer checkForUpdatesTimer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button AppBackgroundCheckButton;
+        private System.Windows.Forms.Button ExplicitUserRequestCheckButton;
+    }
+}
+
---
+++ src/NetSparkle.Samples.NetCore.WinForms/Form1.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using NetSparkle;
+using NetSparkle.Enums;
+
+namespace SampleApplication
+{
+    public partial class Form1 : Form
+    {
+        private Sparkle _sparkleUpdateDetector;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var appcastUrl = "https://deadpikle.github.io/NetSparkle/files/sample-app/appcast.xml";
+            // set icon in project properties!
+            string manifestModuleName = System.Reflection.Assembly.GetEntryAssembly().ManifestModule.FullyQualifiedName;
+            var icon = System.Drawing.Icon.ExtractAssociatedIcon(manifestModuleName);
+            _sparkleUpdateDetector = new Sparkle(appcastUrl, icon)
+            {
+                UIFactory = new NetSparkle.UI.WinForms.UIFactory(),
+                //ShowsUIOnMainThread = true,
+                //UseNotificationToast = true
+            };
+            // TLS 1.2 required by GitHub (https://developer.github.com/changes/2018-02-01-weak-crypto-removal-notice/)
+            _sparkleUpdateDetector.SecurityProtocolType = System.Net.SecurityProtocolType.Tls12;
+            //_sparkleUpdateDetector.CloseApplication += _sparkleUpdateDetector_CloseApplication;
+            _sparkleUpdateDetector.StartLoop(true, true);
+        }
+
+        private void _sparkleUpdateDetector_CloseApplication()
+        {
+            Application.Exit();
+        }
+
+        private async void AppBackgroundCheckButton_Click(object sender, EventArgs e)
+        {
+            // Manually check for updates, this will not show a ui
+            var result = await _sparkleUpdateDetector.CheckForUpdatesQuietly();
+            if (result.Status == UpdateStatus.UpdateAvailable)
+            {
+                // if update(s) are found, then we have to trigger the UI to show it gracefully
+                _sparkleUpdateDetector.ShowUpdateNeededUI();
+            }
+
+        }
+
+        private void ExplicitUserRequestCheckButton_Click(object sender, EventArgs e)
+        {
+            _sparkleUpdateDetector.CheckForUpdatesAtUserRequest();
+        }
+    }
+}
