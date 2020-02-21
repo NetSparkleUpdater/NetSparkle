e26d8e24b4fa78f69d1c291b42fe3a54c6670570
Deadpikle <mbabienco@southern.edu>
2020-02-21 16:28:30 -0500

    Add IconUtilities to WPF UI

---
+++ src/NetSparkle.UI.WPF/IconUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace NetSparkle.UI.WPF
+{
+    public class IconUtilities
+    {
+        /// <summary>
+        /// Convert System.Drawing.Icon to System.Windows.Media.ImageSource.
+        ///  From: https://stackoverflow.com/a/6580799/3938401
+        /// </summary>
+        /// <param name="icon"></param>
+        /// <returns></returns>
+        public static ImageSource ToImageSource(Icon icon)
+        {
+            if (icon == null)
+            {
+                return null;
+            }
+
+            ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            return imageSource;
+        }
+    }
+}
