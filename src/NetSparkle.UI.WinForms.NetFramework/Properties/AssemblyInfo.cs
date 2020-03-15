b2bb76d30f59e178272fa4b20c0539cd197efe4d
Deadpikle <mbabienco@southern.edu>
2020-03-15 18:29:25 -0400

    Update project files

--- src/NetSparkle.UI.WinForms.NetFramework/Properties/AssemblyInfo.cs
+++ src/NetSparkle.UI.WinForms.NetFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetSparkle.NetFramework.WinForms")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
