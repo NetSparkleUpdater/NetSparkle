d010ee3239206f6215593deabb4b93b3a8ac2272
Deadpikle <mbabienco@southern.edu>
2020-04-05 17:25:11 -0400

    Attempt to push NetSparkle.Tools

--- NetSparklePackNuget.cmd
+++ NetSparklePackNuget.cmd
@@ -7,43 +7,43 @@ if "%~1"=="" (
 )
 
 echo Cleanup NuGet environment
-rmdir /S /Q Nuget\core\lib
-rmdir /S /Q Nuget\core\content
-rmdir /S /Q Nuget\core\tools
-del \q Nuget\core\*.nupkg
-rmdir /S /Q Nuget\tools\lib
-rmdir /S /Q Nuget\tools\content
-rmdir /S /Q Nuget\tools\tools
-del \q Nuget\tools\*.nupkg
+rmdir /S /Q nuget\core\lib
+rmdir /S /Q nuget\core\content
+rmdir /S /Q nuget\core\tools
+del \q nuget\core\*.nupkg
+rmdir /S /Q nuget\tools\lib
+rmdir /S /Q nuget\tools\content
+rmdir /S /Q nuget\tools\tools
+del \q nuget\tools\*.nupkg
 
 echo Create NuGet tree for core
-mkdir Nuget\core\lib
-mkdir Nuget\core\content
-mkdir Nuget\core\tools
+mkdir nuget\core\lib
+mkdir nuget\core\content
+mkdir nuget\core\tools
 
 echo Create NuGet tree for tools
-mkdir Nuget\tools\lib
-mkdir Nuget\tools\content
-mkdir Nuget\tools\content\Extras
-mkdir Nuget\tools\tools
+mkdir nuget\tools\lib
+mkdir nuget\tools\content
+mkdir nuget\tools\content\Extras
+mkdir nuget\tools\tools
 
 echo Copy Core Buildoutput to Nuget dir
-xcopy /s /q bin\Release\NetSparkle\* Nuget\core\lib\net45\
-del /q Nuget\core\lib\net45\*.pdb
+xcopy /s /q bin\Release\NetSparkle\* nuget\core\lib\net45\
+del /q nuget\core\lib\net45\*.pdb
 
 echo Copy Tools Buildoutput to Nuget dir
-xcopy /s /q /y bin\Release\NetSparkleChecker\* Nuget\tools\tools\
-xcopy /s /q /y bin\Release\DSAHelper\* Nuget\tools\tools\
-xcopy /s /q /y bin\Release\NetSparkleGenerator\* Nuget\tools\tools\
-xcopy /s /q /y Extras\* Nuget\tools\content\Extras\
-del /q Nuget\tools\tools\*.config
-del /q Nuget\tools\tools\*.pdb
-del /q Nuget\tools\tools\*.xml
-del /q Nuget\tools\tools\*.manifest
-del /q Nuget\tools\tools\*.vshost.*
+xcopy /s /q /y bin\Release\NetSparkleChecker\* nuget\tools\tools\
+xcopy /s /q /y bin\Release\DSAHelper\* nuget\tools\tools\
+xcopy /s /q /y bin\Release\NetSparkleGenerator\* nuget\tools\tools\
+xcopy /s /q /y Extras\* nuget\tools\content\Extras\
+del /q nuget\tools\tools\*.config
+del /q nuget\tools\tools\*.pdb
+del /q nuget\tools\tools\*.xml
+del /q nuget\tools\tools\*.manifest
+del /q nuget\tools\tools\*.vshost.*
 
 echo Moving to release directory
-cd Nuget
+cd nuget
 
 echo Packing core nuget package
 cd core
