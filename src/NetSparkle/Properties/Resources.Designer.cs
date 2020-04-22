152a05cb4ae9a5076f28f605c5150966f8a0bb9c
Deadpikle <mbabienco@southern.edu>
2020-04-22 12:42:37 -0400

    Fixed wrong lookup for resources

--- src/NetSparkle/Properties/Resources.Designer.cs
+++ src/NetSparkle/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace NetSparkleUpdater.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NetSparkle.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NetSparkleUpdater.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
