646b0ee75e6015af780533a360f4cad8f229f640
Deadpikle <mbabienco@southern.edu>
2020-05-12 20:13:04 -0400

    WinForms .NET Framework: Fix MessageNotificationWindow sizing Uses TableLayout for auto sizing Closes #95

--- src/NetSparkle.UI.WinForms.NetFramework/MessageNotificationWindow.Designer.cs
+++ src/NetSparkle.UI.WinForms.NetFramework/MessageNotificationWindow.Designer.cs
@@ -32,14 +32,17 @@ namespace NetSparkleUpdater.UI.WinForms
             this.iconImage = new System.Windows.Forms.PictureBox();
             this.lblMessage = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.iconImage)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // iconImage
             // 
+            this.iconImage.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.iconImage.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.iconImage.Location = new System.Drawing.Point(15, 29);
-            this.iconImage.Margin = new System.Windows.Forms.Padding(6);
+            this.iconImage.Location = new System.Drawing.Point(16, 12);
+            this.iconImage.Margin = new System.Windows.Forms.Padding(6, 12, 6, 6);
             this.iconImage.Name = "iconImage";
             this.iconImage.Size = new System.Drawing.Size(48, 48);
             this.iconImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -50,8 +53,8 @@ namespace NetSparkleUpdater.UI.WinForms
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.Location = new System.Drawing.Point(94, 29);
-            this.lblMessage.MaximumSize = new System.Drawing.Size(630, 130);
+            this.lblMessage.Location = new System.Drawing.Point(83, 0);
+            this.lblMessage.MaximumSize = new System.Drawing.Size(630, 0);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(623, 126);
             this.lblMessage.TabIndex = 8;
@@ -60,7 +63,10 @@ namespace NetSparkleUpdater.UI.WinForms
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(322, 192);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
+            this.button1.Location = new System.Drawing.Point(298, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 46);
             this.button1.TabIndex = 10;
@@ -68,20 +74,39 @@ namespace NetSparkleUpdater.UI.WinForms
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.OK_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.iconImage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblMessage, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.MaximumSize = new System.Drawing.Size(725, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(725, 195);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
             // MessageNotificationWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(751, 290);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.iconImage);
-            this.Controls.Add(this.lblMessage);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(751, 229);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MessageNotificationWindow";
             this.Text = "Message";
             ((System.ComponentModel.ISupportInitialize)(this.iconImage)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,5 +117,6 @@ namespace NetSparkleUpdater.UI.WinForms
         private System.Windows.Forms.PictureBox iconImage;
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
