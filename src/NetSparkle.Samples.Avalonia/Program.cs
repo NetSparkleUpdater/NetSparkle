004a6d14fc8b790d606dba2f978839ee3292568a
Michael Babienco <mbabienco@southern.edu>
2021-05-09 18:42:41 -0400

    Bump versions and fix build warning

--- src/NetSparkle.Samples.Avalonia/Program.cs
+++ src/NetSparkle.Samples.Avalonia/Program.cs
@@ -16,6 +16,6 @@ namespace NetSparkleUpdater.Samples.Avalonia
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToDebug();
+                .LogToTrace();
     }
 }
