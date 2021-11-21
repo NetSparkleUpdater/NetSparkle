8767d0ab2b79350fef2a6ac0f2aa9960e3586b31
Michael Babienco <mbabienco@southern.edu>
2021-11-21 11:20:50 -0500

    Add sample with macOS Avalonia for sudo

--- src/NetSparkle.Samples.Avalonia.MacOS/MainWindow.xaml.cs
+++ src/NetSparkle.Samples.Avalonia.MacOS/MainWindow.xaml.cs
@@ -21,11 +21,12 @@ namespace NetSparkleUpdater.Samples.Avalonia
 #endif
             // set icon in project properties!
             string manifestModuleName = System.Reflection.Assembly.GetEntryAssembly().ManifestModule.FullyQualifiedName;
-            _sparkle = new SparkleUpdater("https://netsparkleupdater.github.io/NetSparkle/files/sample-app-macos/appcast.xml", new Ed25519Checker(Enums.SecurityMode.Strict, "8zPswEwycU7XQ7OcGQtI/b22pWo1qM2Ual2OhssaDyI="))
+            _sparkle = new CustomSparkleUpdater("https://netsparkleupdater.github.io/NetSparkle/files/sample-app-macos/appcast.xml", new Ed25519Checker(Enums.SecurityMode.Strict, "8zPswEwycU7XQ7OcGQtI/b22pWo1qM2Ual2OhssaDyI="))
             {
                 UIFactory = new NetSparkleUpdater.UI.Avalonia.UIFactory(Icon),
                 // Avalonia version doesn't support separate threads: https://github.com/AvaloniaUI/Avalonia/issues/3434#issuecomment-573446972
                 ShowsUIOnMainThread = true,
+                LogWriter = new LogWriter(true)
                 //UseNotificationToast = false // Avalonia version doesn't yet support notification toast messages
             };
             // TLS 1.2 required by GitHub (https://developer.github.com/changes/2018-02-01-weak-crypto-removal-notice/)
