ec7e61feaaf1acfb54a543be31cbf7b5c40a5520
Deadpikle <mbabienco@southern.edu>
2022-11-18 15:11:41 -0500

    Add untested local file downloader
    #373

---
+++ src/NetSparkle/Downloaders/LocalFileAppCastDownloader.cs
@@ -0,0 +1,43 @@
+﻿using NetSparkleUpdater.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSparkleUpdater.Downloaders
+{
+    /// <summary>
+    /// Class that takes care of downloading data for an app cast, including the 
+    /// app cast itself as well as the app cast signature (if available). Allows
+    /// you to send extra JSON with your request for the app cast information.
+    /// </summary>
+    public class LocalFileAppCastDownloader : IAppCastDataDownloader
+    {
+        private string _appcastUrl = "";
+
+        /// <summary>
+        /// Default constructor for the app cast data downloader. Basically
+        /// does nothing. :)
+        /// </summary>
+        public LocalFileAppCastDownloader()
+        {
+        }
+
+        /// <inheritdoc/>
+        public string DownloadAndGetAppCastData(string url)
+        {
+            return File.ReadAllText(url);
+        }
+
+        /// <inheritdoc/>
+        public Encoding GetAppCastEncoding()
+        {
+            return Encoding.UTF8;
+        }
+    }
+}
---
+++ src/NetSparkle/Downloaders/LocalFileDownloader.cs
@@ -0,0 +1,149 @@
+﻿using NetSparkleUpdater.Events;
+using NetSparkleUpdater.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace NetSparkleUpdater.Downloaders
+{
+    /// <summary>
+    /// Class that downloads files from the internet and reports
+    /// progress on those files being downloaded. Uses a <seealso cref="WebClient"/>
+    /// object as its main method for downloading.
+    /// </summary>
+    public class LocalFileDownloader : IUpdateDownloader, IDisposable
+    {
+        private ILogger _logger;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        /// <summary>
+        /// Default constructor for the local file downloader.
+        /// </summary>
+        public LocalFileDownloader()
+        {
+            _logger = new LogWriter();
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        /// Default constructor for the web client file downloader.
+        /// Uses default credentials and default proxy.
+        /// </summary>
+        /// <param name="logger">ILogger to write logs to</param>
+        public LocalFileDownloader(ILogger logger)
+        {
+            _logger = logger;
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        /// ILogger to log data from WebClientFileDownloader
+        /// </summary>
+        public ILogger LogWriter
+        {
+            set { _logger = value; }
+            get { return _logger; }
+        }
+
+        /// <inheritdoc/>
+        public bool IsDownloading { get; private set; }
+
+        /// <inheritdoc/>
+        public event DownloadProgressEvent DownloadProgressChanged;
+        /// <inheritdoc/>
+        public event AsyncCompletedEventHandler DownloadFileCompleted;
+
+        /// <inheritdoc/>
+        public void CancelDownload()
+        {
+            _cancellationTokenSource.Cancel();
+            DownloadFileCompleted?.Invoke(this, new AsyncCompletedEventArgs(null, true, null));
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> RetrieveDestinationFileNameAsync(AppCastItem item)
+        {
+            return await Task.Run(() => Path.GetFileName(item.DownloadLink));
+        }
+
+        /// <inheritdoc/>
+        public async void StartFileDownload(Uri uri, string downloadFilePath)
+        {
+            await CopyFileAsync(uri.AbsolutePath, downloadFilePath, _cancellationTokenSource.Token);
+        }
+
+        // https://stackoverflow.com/a/36925751/3938401 and
+        // https://stackoverflow.com/questions/39742515/stream-copytoasync-with-progress-reporting-progress-is-reported-even-after-cop
+        private async Task CopyFileAsync(string sourceFile, string destinationFile, CancellationToken cancellationToken)
+        {
+            var fileOptions = FileOptions.Asynchronous | FileOptions.SequentialScan;
+            var bufferSize = 4096;
+            var buffer = new byte[bufferSize];
+            int bytesRead = 0;
+            long totalRead = 0;
+            try
+            {
+                using (var sourceStream =
+                      new FileStream(sourceFile, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize, fileOptions))
+                using (var destinationStream =
+                      new FileStream(destinationFile, FileMode.CreateNew, FileAccess.Write, FileShare.None, bufferSize, fileOptions))
+                {
+                    IsDownloading = true;
+                    var wasCanceled = false;
+                    while ((bytesRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+                    {
+                        await destinationStream.WriteAsync(buffer, 0, bytesRead, cancellationToken);
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            Cancel(destinationFile);
+                            wasCanceled = true;
+                            break;
+                        }
+                        totalRead += bytesRead;
+                        UpdateDownloadProgress(totalRead, bytesRead);
+                    }
+                    IsDownloading = false;
+                    DownloadFileCompleted?.Invoke(this, new AsyncCompletedEventArgs(null, wasCanceled, null));
+                }
+            }
+            catch (Exception e)
+            {
+                LogWriter.PrintMessage("Error: {0}", e.Message);
+                Cancel(destinationFile);
+                DownloadFileCompleted?.Invoke(this, new AsyncCompletedEventArgs(e, true, null));
+            }
+        }
+
+        private void Cancel(string destinationFile)
+        {
+            if (File.Exists(destinationFile))
+            {
+                File.Delete(destinationFile);
+            }
+            IsDownloading = false;
+        }
+
+        private void UpdateDownloadProgress(long totalRead, long totalLength)
+        {
+            int percentage = Convert.ToInt32(Math.Round((double)totalRead / totalLength * 100, 0));
+
+            DownloadProgressChanged?.Invoke(this, new ItemDownloadProgressEventArgs(percentage, null, totalRead, totalLength));
+        }
+    }
+}
