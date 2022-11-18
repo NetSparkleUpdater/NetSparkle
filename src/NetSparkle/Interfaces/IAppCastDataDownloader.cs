6762b8902e182487cdd2c72a9ffcfbe114a3e9ba
Deadpikle <mbabienco@southern.edu>
2022-11-18 14:41:45 -0500

    Fix incorrect docs

--- src/NetSparkle/Interfaces/IAppCastDataDownloader.cs
+++ src/NetSparkle/Interfaces/IAppCastDataDownloader.cs
@@ -13,7 +13,7 @@ namespace NetSparkleUpdater.Interfaces
     public interface IAppCastDataDownloader
     {
         /// <summary>
-        /// Used for both downloading app cast and the app cast's .dsa file.
+        /// Used for both downloading app cast and the app cast's .signature file.
         /// Note that you must handle your own exceptions if they occur. 
         /// Otherwise, <see cref="SparkleUpdater"></see> will act as though the appcast 
         /// failed to download.
