2a40ea68e4815249f0bbd3a8054e48c353753763
Michael Babienco <mbabienco@southern.edu>
2023-06-04 10:18:40 -0400

    Add failing unit test for JSONConfig save path

---
+++ src/NetSparkle.Tests/ConfigurationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using NetSparkleUpdater.Configurations;
+using Xunit;
+
+namespace NetSparkleUnitTests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void CanUseCustomJsonConfigPath()
+        {
+            // create tmp file 
+            var path = Path.Combine(Path.GetTempPath(), "SparkleUpdaterTesting");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            try
+            {
+                var jsonConfig = new JSONConfiguration(new FakeTestDataAssemblyAccessor()
+                {
+                    AssemblyCompany = "UnitTest",
+                    AssemblyProduct = "UnitTest"
+                }, path);
+                Assert.Equal(path, jsonConfig.GetSavePath());
+            }
+            finally
+            {
+                // clean up created signature manager even if test fails
+                // get rid of temp file
+                Directory.Delete(path);
+            }
+        }
+    }
+}
