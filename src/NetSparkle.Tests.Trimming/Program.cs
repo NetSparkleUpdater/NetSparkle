33960566a74aa3822c94c4621b6b21201c25dd07
Deadpikle <mbabienco@southern.edu>
2024-07-14 15:27:57 +0900

    Trimming test project updates

--- src/NetSparkle.Tests.Trimming/Program.cs
+++ src/NetSparkle.Tests.Trimming/Program.cs
@@ -1,7 +1,15 @@
-﻿using NetSparkleUpdater.Enums;
+﻿using NetSparkleUpdater;
+using NetSparkleUpdater.Enums;
 using NetSparkleUpdater.SignatureVerifiers;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 var checker = new Ed25519Checker(SecurityMode.Strict, "");
+var sparkle = new SparkleUpdater("https://netsparkleupdater.github.io/NetSparkle/files/sample-app/appcast.xml", new DSAChecker(NetSparkleUpdater.Enums.SecurityMode.Strict))
+            {
+                ShowsUIOnMainThread = false,
+                //RelaunchAfterUpdate = true,
+                //UseNotificationToast = true
+            };
+            await sparkle.CheckForUpdatesQuietly();
