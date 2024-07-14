1c5c88018bd00341138ea70d8e2935eb29b0b384
Deadpikle <mbabienco@southern.edu>
2024-07-14 15:31:15 +0900

    Update tests to compare BouncyCastle with Chaos.NaCl
    Ensures compat now that we've switched

--- src/NetSparkle.Tests/Ed25519Tests.cs
+++ src/NetSparkle.Tests/Ed25519Tests.cs
@@ -2,7 +2,6 @@
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Security;
 using System;
 using System.Collections.Generic;
@@ -17,7 +16,7 @@ namespace NetSparkleUnitTests
         private static readonly SecureRandom Random = new SecureRandom();
 
         [Fact]
-        public void CanValidateSignature()
+        public void CanValidateBouncyCastleSignatureMadeViaKeysFromBouncyCastle()
         {
             Ed25519KeyPairGenerator kpg = new Ed25519KeyPairGenerator();
             kpg.Init(new Ed25519KeyGenerationParameters(Random));
@@ -29,9 +28,68 @@ namespace NetSparkleUnitTests
             // create signature for item
             byte[] msg = new byte[Random.NextInt() & 255];
             Random.NextBytes(msg);
-            var signer = new Ed25519Signer();
+            var signer = new Org.BouncyCastle.Crypto.Signers.Ed25519Signer();
             signer.Init(true, privateKey);
             signer.BlockUpdate(msg, 0, msg.Length);
+            byte[] signature = signer.GenerateSignature();
+            var signatureForAppCast = Convert.ToBase64String(signature);
+
+            // verify signature
+            var checker = new Ed25519Checker(NetSparkleUpdater.Enums.SecurityMode.Strict, pubKeyBase64);
+            Assert.True(checker.VerifySignature(signatureForAppCast, msg) == NetSparkleUpdater.Enums.ValidationResult.Valid);
+        }
+
+        [Fact]
+        public void CanValidateSignatureMadeViaKeysFromChaosNaCl()
+        {
+            var seed = new byte[32];
+            Random.NextBytes(seed);
+            Chaos.NaCl.Ed25519.KeyPairFromSeed(out byte[] publicKey, out byte[] privateKey, seed);
+
+            var pubKeyBase64 = Convert.ToBase64String(publicKey);
+            // create signature for item
+            byte[] msg = new byte[Random.NextInt() & 255];
+            Random.NextBytes(msg);
+            var signer = new Chaos.NaCl.Ed25519Signer();
+            signer.Init(null, privateKey);
+            signer.AddToBuffer(msg, 0, msg.Length);
+            byte[] signature = signer.GenerateSignature();
+            var signatureForAppCast = Convert.ToBase64String(signature);
+
+            // verify signature
+            var checker = new Ed25519Checker(NetSparkleUpdater.Enums.SecurityMode.Strict, pubKeyBase64);
+            Assert.True(checker.VerifySignature(signatureForAppCast, msg) == NetSparkleUpdater.Enums.ValidationResult.Valid);
+        }
+
+        [Fact]
+        public void CanValidateSignatureFromChaosNaClFromBouncyCastleKeys()
+        {
+            Ed25519KeyPairGenerator kpg = new Ed25519KeyPairGenerator();
+            kpg.Init(new Ed25519KeyGenerationParameters(Random));
+
+            AsymmetricCipherKeyPair kp = kpg.GenerateKeyPair();
+            Ed25519PrivateKeyParameters privateKey = (Ed25519PrivateKeyParameters)kp.Private;
+            Ed25519PublicKeyParameters publicKey = (Ed25519PublicKeyParameters)kp.Public;
+            var pubKeyBase64 = Convert.ToBase64String(publicKey.GetEncoded());
+            // create signature for item
+            byte[] msg = new byte[Random.NextInt() & 255];
+            Random.NextBytes(msg);
+            // internally, BouncyCastle is using the seed as its private key. This matches the Chaos.NaCl
+            // documentation that the 32 bit private key is "A 32 byte seeds which allow arbitrary values.
+            // This is the form that should be generated and stored." and the expanded one is
+            // "A 64 byte expanded form. This form is used internally to improve performance"
+            // So BouncyCastle's private key that is saved on disk is "just" the seed.
+            // (See https://github.com/bcgit/bc-csharp/blob/master/crypto/src/crypto/generators/Ed25519KeyPairGenerator.cs)
+            // See also notes on https://github.com/orlp/ed25519 and https://github.com/johndbritton/teleport/pull/84
+            // ("In newer implementations of the algorithm, the seed is saved and the keys are derived from the seed each time they are needed.")
+            // The expanded key basically is the seed and the public key, it looks like, based on crypto_sign2 and
+            // the reference implementation of https://github.com/orlp/ed25519/blob/master/src/sign.c (the latter takes a public key
+            // in its signing algorithm and the code in Chaos.NaCl is similar if not identical, therefore the public key is also
+            // used in Chaos.NaCl. ...yes, this isn't scientific necessarily, but it saves us from reading the implementation algorithm itself.
+            var expandedPrivatekey = Chaos.NaCl.Ed25519.ExpandedPrivateKeyFromSeed(privateKey.GetEncoded());
+            var signer = new Chaos.NaCl.Ed25519Signer();
+            signer.Init(null, expandedPrivatekey);
+            signer.AddToBuffer(msg, 0, msg.Length);
             byte[] signature = signer.GenerateSignature();
             var signatureForAppCast = Convert.ToBase64String(signature);
 
@@ -53,7 +111,7 @@ namespace NetSparkleUnitTests
             // create signature for item
             byte[] msg = new byte[1024*1024*50];
             Random.NextBytes(msg);
-            var signer = new Ed25519Signer();
+            var signer = new Org.BouncyCastle.Crypto.Signers.Ed25519Signer();
             signer.Init(true, privateKey);
             signer.BlockUpdate(msg, 0, msg.Length);
             byte[] signature = signer.GenerateSignature();
