60ccbc26b77bbc910967cf506069879b758dcf2b
Michael Babienco <mbabienco@southern.edu>
2024-08-02 17:20:54 +0900

    Add KeepItemsWithNoSuffix , docs to ChannelAppCastFilter

--- src/NetSparkle/AppCastHandlers/ChannelAppCastFilter.cs
+++ src/NetSparkle/AppCastHandlers/ChannelAppCastFilter.cs
@@ -7,7 +7,8 @@ namespace NetSparkleUpdater.AppCastHandlers
 {
     /// <summary>
     /// Basic <seealso cref="IAppCastFilter"/> implementation for filtering
-    /// your app cast items based on 
+    /// your app cast items based on a channel name (e.g. "beta"). Makes it
+    /// easy to allow your users to be on a beta software track or similar.
     /// </summary>
     public class ChannelAppCastFilter : IAppCastFilter
     {
@@ -20,12 +21,34 @@ namespace NetSparkleUpdater.AppCastHandlers
         public ChannelAppCastFilter(ILogger? logWriter = null)
         {
             RemoveOlderItems = true;
-            ChannelName = "beta";
+            KeepItemsWithNoSuffix = true;
+            ChannelName = "";
             _logWriter = logWriter;
         }
 
+        /// <summary>
+        /// Set to true to remove older items (&lt;= the current installed version);
+        /// false to keep them.
+        /// Defaults to true.
+        /// </summary>
         public bool RemoveOlderItems { get; set; }
+
+        /// <summary>
+        /// Channel name (e.g. "beta" or "alpha" to filter by).
+        /// Defaults to "".
+        /// </summary>
         public string ChannelName { get; set; }
+
+        /// <summary>
+        /// When filtering by <see cref="ChannelName"/>, true to keep items
+        /// that have a version with no suffix (e.g. "1.2.3" only);
+        /// false to get rid of those. Setting this to true will 
+        /// allow users on a beta channel to get updates for the standard
+        /// update channel.
+        /// Has no effect when <see cref="ChannelName"/> is whitespace/empty.
+        /// Defaults to true.
+        /// </summary>
+        public bool KeepItemsWithNoSuffix { get; set; }
 
         /// <inheritdoc/>
         public IEnumerable<AppCastItem> GetFilteredAppCastItems(SemVerLike installed, IEnumerable<AppCastItem> items)
@@ -36,11 +59,15 @@ namespace NetSparkleUpdater.AppCastHandlers
                 var semVer = SemVerLike.Parse(item.Version);
                 if (RemoveOlderItems && semVer.CompareTo(installed) <= 0)
                 {
+                    _logWriter?.PrintMessage("Removing older item from filtered app cast results");
                     return false;
                 }
                 if (!string.IsNullOrWhiteSpace(ChannelName))
                 {
-                    return semVer.AllSuffixes.ToLower().Contains(lowerChannelName);
+                    _logWriter?.PrintMessage("Filtering by channel: {0}; keeping items with no suffix = {1}", 
+                        lowerChannelName, KeepItemsWithNoSuffix);
+                    return semVer.AllSuffixes.ToLower().Contains(lowerChannelName) ||
+                        (KeepItemsWithNoSuffix && string.IsNullOrWhiteSpace(semVer.AllSuffixes.Trim()));
                 }
                 return true;
             }).OrderByDescending(x => x.SemVerLikeVersion);
