a53b7965c894b75d127033f523a7c672f094494d
Deadpikle <mbabienco@southern.edu>
2024-08-17 21:34:39 +0900

    Add Directory.Build.props for easier Avalonia version management

--- src/NetSparkle.UI.Avalonia/Helpers/RelayCommand.cs
+++ src/NetSparkle.UI.Avalonia/Helpers/RelayCommand.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace NetSparkleUpdater.UI.Avalonia.Helpers
 {
