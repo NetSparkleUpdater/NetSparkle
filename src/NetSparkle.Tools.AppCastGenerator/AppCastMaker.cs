e1283496fa3ceb3e7c73225e8f349ce0cc2d1414
Michael Babienco <mbabienco@southern.edu>
2024-10-05 19:14:35 +0900

    Fix app cast gen not working w/multiple dashes
    Closes #623

--- src/NetSparkle.Tools.AppCastGenerator/AppCastMaker.cs
+++ src/NetSparkle.Tools.AppCastGenerator/AppCastMaker.cs
@@ -127,10 +127,10 @@ namespace NetSparkleUpdater.AppCastGenerator
                 // if segment has text in it at the start and digits later, get rid of text and +/- symbol
                 if (Regex.IsMatch(segment, @"[a-zA-Z]") && Regex.IsMatch(segment, @"\d"))
                 {
-                    var match = Regex.Match(segment, @"[^+-]*[a-zA-Z][+-]?");
-                    if (match.Success)
+                    var match = Regex.Matches(segment, @"[^+-]*[a-zA-Z][+-]?");
+                    if (match.Count > 0)
                     {
-                        segment = segment.Substring(match.Index + match.Length);
+                        segment = segment.Substring(match.Last().Index + match.Last().Length);
                         lastVersionToCheck = true;
                     }
                 }
